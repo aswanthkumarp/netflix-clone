443f334cce184e3570bbb7c9c7d80ad56d7e09f2
ASWANTH KUMAR P <122904133+aswanthkumarp@users.noreply.github.com>
2023-10-03 19:58:34 +0530

    Update constants.js

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,3 +1,3 @@
  export const baseurl = 'https://api.themoviedb.org/3';
- export const API_KEY = "4c81c50c0fe6860f57d160a9f3c37b45";
+ export const API_KEY = "Enter your API Key";
  export const imageurl = 'https://image.tmdb.org/t/p/original';
